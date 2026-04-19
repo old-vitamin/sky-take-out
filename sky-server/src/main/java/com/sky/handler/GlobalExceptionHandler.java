39c33e8da6c842fa701a89df2724f896286ce81e
weqi <19938221272@163.com>
2026-04-19 21:34:38 +0800

    新增员工业务开发

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -19,9 +22,35 @@ public class GlobalExceptionHandler {
      * @return
      */
     @ExceptionHandler
-    public Result exceptionHandler(BaseException ex){
+    public Result exceptionHandler(BaseException ex) {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    /**
+     * 处理SQL异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+        // Duplicate entry 'one' for key 'employee.idx_username'
+
+        // 获取到异常是什么.用户名已存在,但重复输入用户名的异常
+        String message = ex.getMessage();
+        // 判断提取到的异常是否是包含内容Duplicate entry
+        if (message.contains("Duplicate entry")) {
+            // 如果包含,输出错误信息
+            // 提取重复的用户名
+            String[] s = message.split(" ");
+            String username = s[2];
+//            String mess = username + "用户已存在,请尝试其它用户名";
+            // 为了规范,使用常量
+            String mess = username + MessageConstant.ALREADY_EXITS;
+            return Result.error(mess);
+        } else {
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
     }
 
 }
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +12,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -53,6 +59,32 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    /**
+     * 新增员工
+     * @param employeeDTO
+     */
+    @Override
+    public void save(EmployeeDTO employeeDTO) {
+        // 将DTO转化为实体类
+        Employee employee = new Employee();
+        // 一次性将DTO属性拷贝到实体类
+        BeanUtils.copyProperties(employeeDTO, employee);
+        // 设置状态
+        employee.setStatus(StatusConstant.ENABLE);
+        // 设置密码(需要加密)
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+        // 设置创建时间
+        employee.setCreateTime(LocalDateTime.now());
+        // 设置更新时间
+        employee.setUpdateTime(LocalDateTime.now());
+        // TODO 还需要修改为当前用户的id --
+        // 记录创建人ID和修改人ID
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
+        // 调用empMapper
+        employeeMapper.insert(employee);
     }
 
 }
